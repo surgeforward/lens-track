7a40f79e7689714346c30b38f3027cedd6191ec2
Mus'ab Husaini <mhusaini@surgeforward.com>
2018-05-22 17:11:51 -0500

    Restructuring the app a bit to have better segmentation.

---
+++ clients/ng-lens-track/src/app/user-profile-management/calendar-view/calendar-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-view',
+  templateUrl: './calendar-view.component.html',
+  styleUrls: ['./calendar-view.component.scss'],
+})
+export class CalendarViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
